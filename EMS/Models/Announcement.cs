8e479ae38a04be41ff2ebb55a2d444581756b5f9
sameepabadhuge <sithusameepa@gmail.com>
2025-12-31 00:24:16 +0530

    again add Announcement model

--- EMS/Models/Announcement.cs
+++ EMS/Models/Announcement.cs
@@ -2,5 +2,17 @@
 {
     public class Announcement
     {
+
+        public int AnnouncementId { get; set; }
+
+        public int ClassId { get; set; }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime DateTime { get; set; }
+
+
+
+
     }
 }

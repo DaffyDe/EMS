a2b23e1a34951e5cb92cca3667008c094b14fcf2
sameepabadhuge <sithusameepa@gmail.com>
2025-12-30 23:52:55 +0530

    modul:update result modul

--- EMS/Models/Result.cs
+++ EMS/Models/Result.cs
@@ -1,6 +1,20 @@
-﻿namespace EMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Models
 {
     public class Result
     {
+
+        
+        public int ResultId { get; set; }
+        
+        public  int StudentId { get; set; }
+
+        public int ClassId { get; set; }
+
+        public required string ExamTitle { get; set; }
+
+        public int Marks { get; set; }
+
     }
 }

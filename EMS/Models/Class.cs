b7b7821b7b077d433d33cbd88711a68739762439
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2025-12-30 22:09:29 +0530

    working progress

---
+++ EMS/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Models
+{
+    public class Class
+    {
+        [Key]
+        public int id { get; set; }
+        public int TeacherId { get; set; }  
+        public Teacher Teacher { get; set; }
+
+
+
+    }
+}
---
+++ EMS/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage ="Name cannot excees 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber is Required")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        [StringLength(10, ErrorMessage = "Phone Number must be 10 digits")]
+        public string PhoneNumber { get; set; }
+
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+    }
+}
